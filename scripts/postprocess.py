75bb283deeb0378ec795f19aed259b048e20c338
uttardakshinvvs <uttar.dakshin.vvs@gmail.com>
2026-02-16 00:01:44 -0500

    Update postprocess.py

--- scripts/postprocess.py
+++ scripts/postprocess.py
@@ -1,25 +1,39 @@
-
 import os, re
 
-# Replace absolute vvsharma.in URLs with relative links and .php -> .html
 origin_hosts = ("http://vvsharma.in", "https://vvsharma.in")
 
-for dirpath, _, files in os.walk('.'):
+def fix_paths(s: str) -> str:
+    # 1) Replace absolute host with relative
+    for host in origin_hosts:
+        s = s.replace(host + "/", "./")
+        s = s.replace(host, ".")
+
+    # 2) Root-absolute → relative for common attributes
+    #    src="/img/..." or href="/img/..." -> "./img/..."
+    s = re.sub(r'(?i)(src|href)\s*=\s*"\/(img\/[^"]+)"', r'\1="./\2"', s)
+
+    # 3) contact.php -> contact.html (Pages is static)
+    s = s.replace("contact.php", "contact.html")
+
+    # 4) Make remaining site-root href="/..." relative
+    s = re.sub(r'(?i)href\s*=\s*"\/(?!\/)', r'href="./', s)
+    s = re.sub(r'(?i)src\s*=\s*"\/(?!\/)', r'src="./', s)
+
+    return s
+
+for dirpath, _, files in os.walk("."):
     for fn in files:
-        if not fn.lower().endswith(('.html','.css','.js','.xml','.txt','.json','.htm')): continue
+        if not fn.lower().endswith((".html",".css",".js",".xml",".txt",".json",".htm")):
+            continue
         p = os.path.join(dirpath, fn)
         try:
-            with open(p,'r',encoding='utf-8',errors='ignore') as f:
+            with open(p, "r", encoding="utf-8", errors="ignore") as f:
                 s = f.read()
         except Exception:
             continue
-        # Absolute to relative for this host
-        for host in origin_hosts:
-            s = s.replace(host+'/', './')
-        # contact.php to contact.html
-        s = s.replace('contact.php', 'contact.html')
-        # Ensure internal anchors don't break
-        s = re.sub(r'href="/(?!/)', 'href="./', s)
-        with open(p,'w',encoding='utf-8') as f:
-            f.write(s)
-print('Postprocess link rewrite done.')
+        ns = fix_paths(s)
+        if ns != s:
+            with open(p, "w", encoding="utf-8") as f:
+                f.write(ns)
+
+print("Postprocess: links rewritten to relative, contact.php -> contact.html")
